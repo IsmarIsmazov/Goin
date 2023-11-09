027a32ee33d2268c59a345eba23e658ca0be20a9
Ismar <ismar110501@gmail.com>
2023-11-09 16:40:34 +0600

    urls

--- Goin/core/urls.py
+++ Goin/core/urls.py
@@ -6,8 +6,9 @@ from django.conf import settings
 from .settings.yasg import urlpatterns_swagger as doc_urls
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/products/', include("apps.products.urls")),
-    path('api/v1/recommendation/', include("apps.recommendation.urls")),
+                  path('admin/', admin.site.urls),
+                  path('api/v1/products/', include("apps.products.urls")),
+                  path('api/v1/recommendation/', include("apps.recommendation.urls")),
+                  path('api/v1/banners/', include("apps.banners.urls"))
 
-] + doc_urls + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ] + doc_urls + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
